d73b6a5ac570ca1dd005da8fa5c65aefbf96dec8
AlirezaGhadyani <alirezaghadyani16@gmail.com>
2021-07-01 14:32:31 +0430

    setting trending pagination and responsive

--- src/Components/Pages/TrendingPage/TrendingPage.js
+++ src/Components/Pages/TrendingPage/TrendingPage.js
@@ -1,20 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import TrendingCard from './TrendingCard';
 import { RiFolderWarningFill } from 'react-icons/ri';
+import ReactPaginate from 'react-paginate';
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
 
 const TrendingPage = () => {
     const [TrendingData, setTrendingData] = useState( [] );
+    const [pageNum, setPageNum] = useState( 1 );
+    const [totalPage, setTotalPage] = useState( 0 );
+
     const fetchTrending = async () => {
-        const response = await fetch( `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_MOVIE_API_KEY}` );
-        const data = await response.json();
-        setTrendingData( data.results );
+        try {
+            const response = await fetch( `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&page=${pageNum}` );
+            const data = await response.json();
+            setTrendingData( data.results );
+            setTotalPage( data.total_pages )
+
+        } catch ( error ) {
+            console.log( error );
+        }
+    }
+
+    const pageChange = ( { selected } ) => {
+        setPageNum( selected + 1 );
+        window.scroll( 0, 0 );
     }
 
     useEffect( () => {
-        fetchTrending()
-    }, [] );
-
-    console.log( TrendingData )
+        fetchTrending();
+    }, [pageNum] );
 
     return (
         <>
@@ -30,6 +44,18 @@ const TrendingPage = () => {
                     )
                 }
             </div>
+
+            <ReactPaginate
+                pageCount={totalPage}
+                pageRangeDisplayed={2}
+                nextLabel={<GrFormNext />}
+                previousLabel={<GrFormPrevious />}
+                onPageChange={pageChange}
+                containerClassName="pagination-wrapper"
+                previousLinkClassName="pagination-prev-btn"
+                nextLinkClassName="pagination-next-btn"
+                disabledClassName="disable-pagination"
+                activeClassName="active-pagination" />
         </>
 
     )
--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -42,6 +42,54 @@ h1,h2,h3,h4,h5,h6,p,span{
 }
 }
 
+.trending-card{
+  background:${( { theme } ) => theme.bgSec};
+}
+
+.t-year,
+.t-type {
+  color:${( { theme } ) => theme.textColorSec};
+}
+
+.t-rate{
+  background:${( { theme } ) => theme.primary};
+  color: ${( { theme } ) => theme.bgSec};
+}
+
+.pagination-wrapper li{
+  color:${( { theme } ) => theme.textColorSec};
+}
+
+.pagination-prev-btn,
+.pagination-next-btn{
+  color:${( { theme } ) => theme.textColor};
+}
+
+.active-pagination{
+  background:${( { theme } ) => theme.textColor};
+  padding:.2rem .4rem;
+  color:${( { theme } ) => theme.bg}!important;
+}
+
+@media (min-width: 531px){
+  .pagination-wrapper li {
+  background:${( { theme } ) => theme.bgSec};
+}
+
+.pagination-prev-btn,
+.pagination-next-btn {
+  @include flex(row, center, center);
+  padding: 0.1rem;
+}
+
+.active-pagination{
+  background:${( { theme } ) => theme.textColor};
+  padding:.2rem .4rem;
+  color:${( { theme } ) => theme.primary}!important;
+}
+
+}
+
 `;
 
 export const lightTheme = {
@@ -57,5 +105,5 @@ export const darkTheme = {
   textColor: '#ffffff',
   bgSec: '#3a3a3a',
   textColorSec: '#d7d7d7',
-  primary: '#c1e8d5'
+  primary: '#8be7bb'
 };
